e1dd50c5a5999c8c68899d5174d683224e331e54
praveen097 <darlingpraveen097@gmail.com>
2021-09-06 01:38:32 +0530

    Added logic to change between the sections

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -12,18 +12,25 @@ import { CostEstimationService } from '../cost-estimation.service';
 export class QuestionsComponent implements OnInit {
   public questions:Questions[] = [];
   public sections:Sections[] = [];
-  currentQuestion=0;
-  questionIncrement(){
-    this.currentQuestion++;
-  }
-  questionDecrement(){
-    this.currentQuestion--;
-  }
+
+  currentQuestion:number = 0;
+  answerSelected:boolean =  false;
+  currentSection:number = 0;
+  qid:number = 0;
+  
   constructor(private _costEstimationService: CostEstimationService) { }
 
   ngOnInit(): void {
     this.questions = this._costEstimationService.getQuestions();
     this.sections =  this._costEstimationService.getSections();
   }
-
+  
+  changeSection(){
+    this.currentSection++;
+    this.qid = 0;
+  }
+  changeQuestion(){
+    this.qid++;
+    this.currentQuestion++;
+  }
 }

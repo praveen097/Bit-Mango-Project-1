7e99b1769c05050f59b7aff7a191a8bfb5b5b56e
Shreyansh Desai <shreyansh@lemon.be>
2021-09-16 13:27:10 +0530

    test commit
    asdf

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -6,26 +6,31 @@ import { Questions } from '../models/questions';
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.css']
+  styleUrls: ['./results.component.css'],
 })
 export class ResultsComponent implements OnInit {
-  minPrice:number = 0;
-  maxPrice:number = 0;
-  minDays:number = 0;
-  maxDays:number = 0;
-  allAnswer:Questions[] =[];
-  showResults:boolean = false;
-  resultExist:boolean = true;
+  minPrice: number = 0;
+  maxPrice: number = 0;
+  minDays: number = 0;
+  maxDays: number = 0;
+  //hello changes
+  allAnswer: Questions[] = [];
+  showResults: boolean = false;
+  resultExist: boolean = true;
 
-  resultText:string[]=['Minimum Price $','Maximum Price $','Minimum Days','Maximum Days'];
-  resultValue:number[] = [];
+  resultText: string[] = [
+    'Minimum Price $',
+    'Maximum Price $',
+    'Minimum Days',
+    'Maximum Days',
+  ];
+  resultValue: number[] = [];
 
+  constructor(
+    private _costEstimationService: CostEstimationService,
+    private route: Router
+  ) {}
 
-  constructor( 
-    private _costEstimationService:CostEstimationService,
-    private route:Router
-  ) { }
-  
   ngOnInit(): void {
     this.maxPrice = this._costEstimationService.maxPrice;
     this.minPrice = this._costEstimationService.minPrice;
@@ -33,15 +38,18 @@ export class ResultsComponent implements OnInit {
     this.minDays = this._costEstimationService.minDays;
 
     this.allAnswer = this._costEstimationService.overAllAnswers;
-    if(this.allAnswer.length == 0){
-     this.resultExist = false;
+    if (this.allAnswer.length == 0) {
+      this.resultExist = false;
     }
-    this.resultValue = [this.minPrice,this.maxPrice,this.minDays,this.maxDays];
+    this.resultValue = [
+      this.minPrice,
+      this.maxPrice,
+      this.minDays,
+      this.maxDays,
+    ];
   }
 
   toResults(): void {
-    this.showResults = true
+    this.showResults = true;
   }
-
-
 }

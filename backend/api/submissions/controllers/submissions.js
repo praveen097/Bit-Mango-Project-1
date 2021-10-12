d52be4c9ee2c5115991b1385023a7e0d313c7302
Chethan Sai K <chethansaikaranam@gmail.com>
2021-10-12 09:40:10 +0530

    Modified Post Submissions Method

--- backend/api/submissions/controllers/submissions.js
+++ backend/api/submissions/controllers/submissions.js
@@ -5,4 +5,27 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    console.log("In Create");
+    let entity;
+    let minPrice = 0;
+    let maxPrice = 0;
+    let answeredQuestions = ctx.request.body.answeredQuestions;
+    answeredQuestions.forEach(question => {
+     question.options.forEach((option)=>{
+       minPrice+=option.minPrice;
+       maxPrice+=option.maxPrice;
+     })
+   });
+    let submitData = {
+      email:ctx.request.body.email,
+      companyName:ctx.request.body.companyName,
+      answeredQuestions:answeredQuestions,
+      lowerEstimate:minPrice,
+      upperEstimate:maxPrice,
+    }
+   entity = await strapi.services.submissions.create(submitData);
+   return submitData;
+  }
+};

7e4a1a0e50b99a45928ed292d6e50a88e21c19b1
Chethan Sai K <chethansaikaranam@gmail.com>
2021-11-03 12:17:00 +0530

    Added NodeMailer

--- backend/api/submissions/controllers/submissions.js
+++ backend/api/submissions/controllers/submissions.js
@@ -4,18 +4,44 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+var nodeMailer = require('nodemailer');
+var transporter = nodeMailer.createTransport({
+  service:'gmail',
+  auth:{
+    user:'',
+    pass:""
+  }
+});
+
 
 module.exports = {
     async create(submission){
         let minPrice = 0;
         let maxPrice = 0;
+        let mailBody = "Hi " + submission.request.body.companyName+"\n\n Greetings from Bit Mango\nHere are the Questions you answered\n\n";
         let answeredQuestions = submission.request.body.answeredQuestions;
         answeredQuestions.forEach(questions => {
+            mailBody+=questions.questionText+"\n";
             questions.options.forEach((option)=>{
                 minPrice += option.minPrice;
-                maxPrice += option.maxPrice;
+                maxPrice += option.maxPrice
+                mailBody+= option.optionText+" ";
             })
+            mailBody+="\n\n"
         });
+        mailBody+='\n\n The Lower Estimate of your project is:'+minPrice+"\n\n The Higher Estimate of your project is :"+maxPrice;
+        var mailOptions = {
+          to:submission.request.body.email,
+          subject:"Cost Estimator - Answered Questions",
+          text:mailBody
+        }
+
+        transporter.sendMail(mailOptions,(error,info)=>{
+          if (error) console.log(error);
+          else{
+            console.log("Email Sent "+info.response);
+          }
+        })
         let dataSubmitted = {
             email:submission.request.body.email,
             companyName:submission.request.body.companyName,

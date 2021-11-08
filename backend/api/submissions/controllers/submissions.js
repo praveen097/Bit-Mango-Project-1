7432b17ce96c5811ab383ae87fba5a88be3c9884
Praveen Kumar <darlingpraveen097@gmail.com>
2021-11-08 15:54:37 +0530

    Added email template

--- backend/api/submissions/controllers/submissions.js
+++ backend/api/submissions/controllers/submissions.js
@@ -4,17 +4,17 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
-var nodemailer = require('nodemailer');
+var nodeMailer = require('nodemailer');
 var handleBars = require('handlebars');
 var fs = require('fs');
 var path = require('path')
-// var transporter = nodeMailer.createTransport({
-//   service:'gmail',
-//   auth:{
-//     user:'',
-//     pass:''
-//   }
-// });
+var transporter = nodeMailer.createTransport({
+  service:'gmail',
+  auth:{
+    user:'testpraveen097@gmail.com',
+    pass:'Pravtest321@'
+  }
+});
 
 
 module.exports = {
@@ -35,68 +35,27 @@ module.exports = {
           subject:"Cost Estimator - Answered Questions",
           attachments: [{
             filename: 'logo.png',
-              path: __dirname +'/logo.png',
-             cid: 'imagename'
+            path: __dirname +'/logo.png',
+            cid: 'imagename'
       }],
           html:template({companyName:submission.request.body.companyName,questions:answeredQuestions,lowerEstimate:minPrice,upperEstimate:maxPrice})
         }
 
-      //   await transporter.sendMail(mailOptions,(error,info)=>{
-      //     if (error) console.log(error);
-      //     else{
-      //       console.log("Email Sent "+info.response);
-      //     }
-      //   })
-      //   let dataSubmitted = {
-      //       email:submission.request.body.email,
-      //       companyName:submission.request.body.companyName,
-      //       lowerEstimate:minPrice,
-      //       upperEstimate:maxPrice,
-      //       answeredQuestions:answeredQuestions
-      //   }
-      //   await strapi.services.submissions.create(dataSubmitted);
-      //   return ({lowerEstimate:minPrice,upperEstimate:maxPrice});
-
-// Generate SMTP service account from ethereal.email
-nodemailer.createTestAccount((err, account) => {
-    if (err) {
-        console.error('Failed to create a testing account. ' + err.message);
-        return process.exit(1);
+        await transporter.sendMail(mailOptions,(error,info)=>{
+          if (error) console.log(error);
+          else{
+            console.log("Email Sent "+info.response);
+          }
+        })
+        let dataSubmitted = {
+            email:submission.request.body.email,
+            companyName:submission.request.body.companyName,
+            lowerEstimate:minPrice,
+            upperEstimate:maxPrice,
+            answeredQuestions:answeredQuestions
+        }
+        //await strapi.services.submissions.create(dataSubmitted);
+        return ({lowerEstimate:minPrice,upperEstimate:maxPrice});
     }
 
-    console.log('Credentials obtained, sending message...');
-
-    // Create a SMTP transporter object
-    let transporter = nodemailer.createTransport({
-        host: account.smtp.host,
-        port: account.smtp.port,
-        secure: account.smtp.secure,
-        auth: {
-            user: account.user,
-            pass: account.pass
-        }
-    });
-
-    // Message object
-    let message = {
-        from: 'Sender Name <sender@example.com>',
-        to: 'Recipient <recipient@example.com>',
-        subject: 'Nodemailer is unicode friendly ✔',
-        text: 'Hello to myself!',
-        html: '<p><b>Hello</b> to myself!</p>'
-    };
-
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            console.log('Error occurred. ' + err.message);
-            return process.exit(1);
-        }
-
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-    });
-  });
-}
-}
+};
